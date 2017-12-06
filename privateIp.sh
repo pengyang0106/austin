6618e8dea1dde0e7fe487e3a71e3ea449a2d96f3
Neil Shannon <neil.shannon@forcepoint.com>
2017-12-06 17:15:59 -0500

    private ip script

---
+++ privateIp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# This script registers the current appliance VM with Triton Manager on-premise
+# via a POST to the REST API
+#
+
+new_ip_address=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1);
+
+my_hostname=`hostname -s`;
+
+tritonIp=$1;
+
+data_to_send="{\"privateIpAddress\": \"$new_ip_address\", \"hostname\":\"$my_hostname\", \"tritonIp\":\"$tritonIp\"}";
+
+logger data_to_send;
+
+curl -k -v -H "Content-Type: application/json" -X POST -d "$data_to_send https://$tritonIp/api/registerApplicance";
+
